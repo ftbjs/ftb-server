fb89ebb787a0901249939b0a6ccb7274eeb95825
BiYuqi <biyuqiwan@163.com>
2020-04-08 00:41:14 +0800

    docs: add dynamic import for example

--- example/src/index.js
+++ example/src/index.js
@@ -1,3 +1,6 @@
 import './index.scss'
 
+if (true) {
+  import(/* webpackChunkName: "dynamic-test" */ './test')
+}
 document.querySelector('#app').innerHTML = '<h1>Goodbye 2020</h1>\n<h1>Hello 2021</h1>'
