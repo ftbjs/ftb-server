b26f4a33724e48264023d3ff51d4f01ad30a7822
BiYuqi <biyuqiwan@163.com>
2020-04-18 13:17:22 +0800

    docs: add antv/g2 demo

--- example/project/src/index.js
+++ example/project/src/index.js
@@ -1,6 +1,3 @@
+import './dynamic-bubble-chart'
+import './dynamic-bar-chart'
 import './index.scss'
-
-if (true) {
-  import(/* webpackChunkName: "dynamic-test" */ './test')
-}
-document.querySelector('#app').innerHTML = '<h1>Goodbye 2020</h1>\n<h1>Hello 2021</h1>'
