02928dca67b81890144f5b1486b1b944110ccabb
BiYuqi <biyuqiwan@163.com>
2020-04-19 00:22:14 +0800

    docs: optimize the library example

--- example/library/src/index.js
+++ example/library/src/index.js
@@ -1,3 +1,7 @@
-import './index.scss'
+export function sum(x, y) {
+  return x + y
+}
 
-document.querySelector('body').innerHTML = '<h1>Goodbye 2020</h1>\n<h1>Hello 2021</h1>'
+if (process.env.NODE_ENV === 'development') {
+  document.querySelector('body').innerHTML = '<h1>Goodbye 2020</h1>\n<h1>Hello 2021</h1>'
+}
