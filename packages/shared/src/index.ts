63a192ec43c335a66d0450a91fc2846824805ec4
BiYuqi <biyuqiwan@163.com>
2020-04-18 23:59:28 +0800

    chore: Add boxen for creating boxes in the terminal

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,5 +2,6 @@ import { logger } from './logger'
 import { findExistSync } from './findExistSync'
 import { createSchema, validate } from './validate'
 import { spinner } from './spinner'
+import { boxen } from './boxen'
 
-export { logger, findExistSync, createSchema, validate, spinner }
+export { logger, findExistSync, createSchema, validate, spinner, boxen }
