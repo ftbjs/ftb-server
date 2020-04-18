38979f203b9015b5ed692bf71fd9534694ec741e
BiYuqi <biyuqiwan@163.com>
2020-04-18 12:45:02 +0800

    docs: add example for create js library

---
+++ example/library/ftb.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  devServer: {
+    port: 2021,
+    open: true
+  },
+  packages: {
+    filename: 'my-test',
+    library: 'myTest',
+    libraryTarget: 'umd'
+  }
+}
---
+++ example/library/src/index.js
@@ -0,0 +1,3 @@
+import './index.scss'
+
+document.querySelector('body').innerHTML = '<h1>Goodbye 2020</h1>\n<h1>Hello 2021</h1>'
